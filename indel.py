f6054ab6bceadd91b493e5783420cd320bc0aa72
brainfo <1248276548@qq.com>
2024-02-22 09:33:28 +0100

    v1 of indel functionalities

--- indel.py
+++ indel.py
@@ -3,21 +3,52 @@
 import pyhgvs as hgvs
 import random
 random.seed(404)
+from pyhgvs.utils import read_transcripts
+from pyfaidx import Fasta
+
+# Read genome sequence using pyfaidx.
+# Read RefSeq transcripts into a python dict.
+def read_genome(genome_file, refgene_file):
+    genome = Fasta(genome_file)
+    with open(refgene_file) as infile:
+        transcripts = read_transcripts(infile)
+    return genome, transcripts
 
 # Provide a callback for fetching a transcript by its name.
 def get_transcript(name):
     return transcripts.get(name)
+
+## get codon fasta
+def index_containing_substring(the_list, substring):
+    for i, s in enumerate(the_list):
+        if substring in s:
+              return i
+    return -1
+
+def get_transcript_fa(ref_fa, transcript):
+    with open(ref_fa) as fi:
+        line_list = fi.readlines()
+        line_idx = index_containing_substring(line_list, transcript)
+        print(line_list[line_idx])
+        line_start ='a'
+        coding_fa = ''
+        while line_start != '>':
+            coding_fa = coding_fa+ line_list[line_idx+1].strip()
+            line_idx += 1
+            line_start = line_list[line_idx+1][0]
+    return coding_fa
 
 def get_codons(coding_fa):
     with open(coding_fa) as fi:
         coding_seq = ''.join(''.join(fi.readlines()[1:]).split('\n'))
     return [coding_seq[i:i+3] for i in range(0,len(coding_seq), 3)]
 
+
 # Parse the HGVS name into genomic coordinates and alleles.
 ## create HGVS names
 
 
-def mk_cins(codon, ng, genome, transcripts):
+def mk_cins(codon, ng):
     '''
     yield c. ins hgvs names
     '''
@@ -29,6 +60,37 @@ def mk_cins(codon, ng, genome, transcripts):
         ins_hgvs = f'{ng}:c.{start}_{end}ins{ins}'
         yield ins_hgvs, i
         i += 1
+
+def mk_cdel(codon, len_transcript, ng):
+    '''
+    yield c. del hgvs names
+    '''
+    i = 1
+    while True:
+        th = random.sample(range(1, len_transcript//3-3), 1)[0]
+        start = th*3-1
+        end = start +2
+        del_seq = codon[th]
+        del_hgvs = f'{ng}:c.{start}_{end}del{del_seq}'
+        yield del_hgvs, i
+        i += 1
+
+## write out indels
+def write_out_indels(out_vcf_file, num_ins, num_del, genome, get_transcript):
+    with open(out_vcf_file, 'a') as out_vcf:
+        out_vcf.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n')
+        for ins_names, num in mk_cins():
+            if num > num_ins:
+                break
+            chrom, offset, ref, alt = hgvs.parse_hgvs_name(ins_names, genome, get_transcript=get_transcript, lazy=True)
+            out_vcf.write(f'{chrom}\t{offset}\t{ins_names}\t{ref}\t{alt}\t.\tPASS\t.\n')
+        for del_names, num in mk_cdel():
+            if num > num_del:
+                break
+            chrom, offset, ref, alt = hgvs.parse_hgvs_name(del_names, genome, get_transcript=get_transcript, lazy=True)
+            out_vcf.write(f'{chrom}\t{offset}\t{del_names}\t{ref}\t{alt}\t.\tPASS\t.\n')
+
+## protein level
 
 one2all ={'A': ('A', 'ALA', 'alanine'),
     'R': ('R', 'ARG', 'arginine'),
@@ -77,3 +139,4 @@ def mk_del(fa, random_del, deletions):
         deli = f'p.{aa1}{i}del'
         deletions.append(deli)
     return deletions
+
